3c036e7edc047237860bd0be3bf75fd31a27293c
TrungTinns <trungtin073@gmail.com>
2023-03-28 00:05:32 +0700

    add form template method pakage

---
+++ src/formtemplatemethod/beforechange/Main.java
@@ -0,0 +1,10 @@
+package formtemplatemethod.beforechange;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.playCricket();
+        System.out.println();
+        game.playFootball();
+    }
+}
