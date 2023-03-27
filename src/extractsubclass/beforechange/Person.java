b4157e9dcb86cc381055aa51f13a7a1ea91b62ca
TrungTinns <trungtin073@gmail.com>
2023-03-27 22:50:24 +0700

    extract subclass

---
+++ src/extractsubclass/beforechange/Person.java
@@ -0,0 +1,11 @@
+package extractsubclass.beforechange;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getDescription() {
+        return "Name: " + name + ", " + "Age: " + age;
+    }
+}
+
